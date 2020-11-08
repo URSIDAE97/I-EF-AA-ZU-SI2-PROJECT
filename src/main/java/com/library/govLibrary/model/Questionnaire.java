a5f2dc68d9f5ca12031075e3d34d7e858ddaee8d
kamil.malita@gmail.com <kamil.malita@gmail.com>
2020-11-08 19:22:51 +0100

    Added some Annotation to questionnaire for validate questionnaire

--- src/main/java/com/library/govLibrary/model/Questionnaire.java
+++ src/main/java/com/library/govLibrary/model/Questionnaire.java
@@ -2,8 +2,10 @@ package com.library.govLibrary.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,13 +17,21 @@ public class Questionnaire {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     @NotNull
     private long idCategory;
     private String username;
+
     @NotNull
+    @Length(min = 5, max = 80)
+    @NotBlank
     private String title;
+
     @NotNull
+    @Length(min = 5, max = 500)
+    @NotBlank
     private String description;
+
     @NotNull
     private LocalDateTime created;
     private LocalDateTime activation;
