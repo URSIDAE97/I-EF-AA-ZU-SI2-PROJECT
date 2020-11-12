f6c6fbb04261518eb02ec2a8dfeee9d4f43a6e29
kamil <kamil.malita@gmail.com>
2020-11-12 21:33:04 +0100

    Add autogenerated id when inserted, Add Option Repository

--- src/main/java/com/library/govLibrary/model/Option.java
+++ src/main/java/com/library/govLibrary/model/Option.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -14,6 +16,7 @@ import javax.validation.constraints.NotNull;
 @Setter
 public class Option {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @NotEmpty
     @NotNull
