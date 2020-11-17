5718c81d7b98a2d6c64c924de51707121aeaaf99
URSIDAE <mateusz.m97@gmail.com>
2020-11-17 18:21:35 +0100

    interceptiors fix

--- src/main/frontend/src/services/axios_config.js
+++ src/main/frontend/src/services/axios_config.js
@@ -13,11 +13,13 @@ server_api.interceptors.request.use((config) => {
   if (token) {
     config.headers.common.applicationtoken = token
   }
+  return config
 })
 
 server_api.interceptors.response.use((response) => {
   const token = response.headers['applicationtoken']
   setAuthToken(token)
+  return response
 })
 
 export default server_api
